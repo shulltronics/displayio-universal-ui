af660317a243458a83e0c2c907e89de0d4374c68
Thue-bludworth <carstentb@ufl.edu>
2022-04-15 13:13:36 -0400

    moved hw independent code to gui.py, created separate files for hw dependent parts

--- code-desktop.py
+++ code-desktop.py
@@ -1,18 +1,8 @@
 ### An attempt to get Blinka Displayio working on Generic x86 PC
 
 import time
-
 from pygame_display import PygameDisplay
-from unigui import UniGui
-
-WIDTH = 320
-HEIGHT = 240
-SCALE_FACTOR = 2
-NUM_COLORS = 2
-
-gui = UniGui(WIDTH, HEIGHT, scale_factor=SCALE_FACTOR)
-gui.set_title("hello carsten")
-gui.set_main_area()
+from gui import gui, WIDTH, HEIGHT, SCALE_FACTOR
 
 print("starting...")
 
@@ -21,12 +11,6 @@ display = PygameDisplay(WIDTH*SCALE_FACTOR, HEIGHT*SCALE_FACTOR)
 gui.update(display)
 
 while(1):
-    pass
-    # x = randint(0, 150)
-    # y = randint(50, 250)
-    # ta.x = x
-    # ta.y = y
-    # display.show(ta)
     time.sleep(0.1)
     event = display.refresh()
     if event == 1:
---
+++ gui.py
@@ -0,0 +1,10 @@
+from unigui import UniGui
+
+WIDTH = 320
+HEIGHT = 240
+SCALE_FACTOR = 2
+NUM_COLORS = 2
+
+gui = UniGui(WIDTH, HEIGHT, scale_factor=SCALE_FACTOR)
+gui.set_title("hello carsten")
+gui.set_main_area()
